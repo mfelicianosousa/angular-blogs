4ee2d66e29ef8a9ef3b3281e7c91bbca4c391f89
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-07-14 18:34:13 -0400

    components dinamics propertys

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './small-card.component.css'
 })
 export class SmallCardComponent {
-
+   @Input() photoCover: string ="";
+   @Input() cardDate: string = "";
+   @Input() cardTitle: string = "";
 }
